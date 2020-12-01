bdbecccc0ac65452bc276d82d57f50ae26ffe6c0
zhaobao1830 <zgt..5219438>
2020-12-01 16:51:15 +0800

    初始化项目

---
+++ src/store.js
@@ -0,0 +1,17 @@
+import {
+  createStore,
+  combineReducers,
+  applyMiddleware
+} from 'redux'
+import thunk from 'redux-thunk';
+import reducers from "./reducers";
+
+const store = createStore(
+  combineReducers(reducers),
+  {
+
+  },
+  applyMiddleware(thunk)
+  );
+
+export default store;
